f85280f0fdec89462ce46d23486aadd3866eab47
Arjun Narayanan <anarayanan86@gmail.com>
2016-09-23 08:41:08 -0700

    Create Exercise:_while_exercise_1.py

---
+++ Week_1-_Python_Basics/2.-Core_Elements_of_Programs/Exercise:_while_exercise_1.py
@@ -0,0 +1,24 @@
+# Exercise: while exercise 1
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# In this problem you'll be given a chance to practice writing some while loops.
+
+# 1. Convert the following into code that uses a while loop.
+
+# print 2
+# prints 4
+# prints 6
+# prints 8
+# prints 10
+# prints Goodbye!
+
+
+None
+
+x = 2
+while x < 11:
+    print(x)
+    x += 2
+print('Goodbye!')
