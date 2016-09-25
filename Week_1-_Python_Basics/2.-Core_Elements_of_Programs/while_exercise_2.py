7e4575b4b02e5af686fbd921508159e812445cbc
Arjun Narayanan <anarayanan86@gmail.com>
2016-09-25 09:31:22 -0700

    Add files via upload

---
+++ Week_1-_Python_Basics/2.-Core_Elements_of_Programs/while_exercise_2.py
@@ -0,0 +1,24 @@
+# Exercise: while exercise 2
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# 2. Convert the following into code that uses a while loop.
+
+# prints Hello!
+# prints 10
+# prints 8
+# prints 6
+# prints 4
+# prints 2
+
+
+None
+
+y = 10
+print('Hello!')
+while y > 0:
+    print(y)
+    y -= 2
+
+# Correct
