c28368ea1a6dd44e09789e113349a557abeb221f
Arjun Narayanan <anarayanan86@gmail.com>
2016-09-26 08:22:09 -0700

    Add files via upload

---
+++ Week_1-_Python_Basics/2.-Core_Elements_of_Programs/while_exercise_3.py
@@ -0,0 +1,26 @@
+# Exercise: while exercise 3
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# 3. Write a while loop that sums the values 1 through end, inclusive. end is a variable that we define for you. So, for example, if we
+# define end to be 6, your code should print out the result:
+
+# 21
+# which is 1 + 2 + 3 + 4 + 5 + 6.
+
+# For problems such as these, do not include input statements or define variables we will provide for you. Our automating testing will
+# provide values so write your code in the following box assuming these variables are already defined.
+
+
+None
+
+total = 0
+i = 0
+while i <= end:
+    total += i
+    i += 1
+
+print(total)
+
+# Correct
