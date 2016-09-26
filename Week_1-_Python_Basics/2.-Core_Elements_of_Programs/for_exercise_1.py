dabf7339de3c449d9b0cb46228e5ea6f46f60d31
Arjun Narayanan <anarayanan86@gmail.com>
2016-09-26 08:22:55 -0700

    Add files via upload

---
+++ Week_1-_Python_Basics/2.-Core_Elements_of_Programs/for_exercise_1.py
@@ -0,0 +1,23 @@
+# Exercise: for exercise 1
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# In this problem you'll be given a chance to practice writing some for loops.
+
+# 1. Convert the following code into code that uses a for loop.
+
+# prints 2
+# prints 4
+# prints 6
+# prints 8
+# prints 10
+# prints "Goodbye!"
+
+None
+
+for i in range(2, 11, 2):
+    print(i)
+print('Goodbye!')
+
+# Correct
