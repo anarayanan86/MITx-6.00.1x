7e86fed6204c11160fecd5fe70a82d22acd42c6a
Arjun Narayanan <anarayanan86@gmail.com>
2016-09-27 08:41:59 -0700

    Add files via upload

---
+++ Week_1-_Python_Basics/2.-Core_Elements_of_Programs/for_exercise_2.py
@@ -0,0 +1,22 @@
+# Exercise: for exercise 2
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# 2. Convert the following code into code that uses a for loop.
+
+# prints "Hello!"
+# prints 10
+# prints 8
+# prints 6
+# prints 4
+# prints 2
+
+
+None
+
+print('Hello!')
+for i in range(10, 0, -2):
+    print(i)
+
+# Correct
