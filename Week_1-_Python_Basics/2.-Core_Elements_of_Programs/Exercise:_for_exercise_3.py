7b965dddbebd9a3a149c7b95f9f1da0bb1531042
Arjun Narayanan <anarayanan86@gmail.com>
2016-09-28 08:14:59 -0700

    Update Exercise:_for_exercise_3.py

--- Week_1-_Python_Basics/2.-Core_Elements_of_Programs/Exercise:_for_exercise_3.py
+++ Week_1-_Python_Basics/2.-Core_Elements_of_Programs/Exercise:_for_exercise_3.py
@@ -3,13 +3,13 @@
 # (5/5 points)
 # ESTIMATED TIME TO COMPLETE: 5 minutes
 
-# 3. Write a for loop that sums the values 1 through end, inclusive. end is a variable that we define for you. So, for example, if we define
-# end to be 6, your code should print out the result:
+# 3. Write a for loop that sums the values 1 through end, inclusive. end is a variable that we define for you. So, for example, if we
+# define end to be 6, your code should print out the result:
 # 21
 # which is 1 + 2 + 3 + 4 + 5 + 6.
 
-# For problems such as these, do not include input statements or define variables we will provide for you. Our automating testing will provide
-# values so write your code in the following box assuming these variables are already defined.
+# For problems such as these, do not include input statements or define variables we will provide for you. Our automating testing will
+# provide values so write your code in the following box assuming these variables are already defined.
 
 
 None
