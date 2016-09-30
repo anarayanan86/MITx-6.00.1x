f47d5a4080989a976633077b88562d7c79c25ba2
Arjun Narayanan <anarayanan86@gmail.com>
2016-09-30 08:41:23 -0700

    Update Problem_3.py

--- Week_1-_Python_Basics/Problem_Set_1/Problem_3.py
+++ Week_1-_Python_Basics/Problem_Set_1/Problem_3.py
@@ -11,8 +11,8 @@
 # Longest substring in alphabetical order is: abc
 
 # Note: This problem may be challenging. We encourage you to work smart. If you've spent more than a few hours on this problem, we suggest
-# that you move on to a different part of the course. If you have time, come back to this problem after you've had a break and cleared your
-# head.
+# that you move on to a different part of the course. If you have time, come back to this problem after you've had a break and cleared
+# your head.
 
 
 # Paste your code into this box
