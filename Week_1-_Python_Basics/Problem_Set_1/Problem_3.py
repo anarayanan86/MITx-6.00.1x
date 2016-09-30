22c6a8f52960a14fc3e8d4cf431d6e9962051599
Arjun Narayanan <anarayanan86@gmail.com>
2016-09-30 08:41:47 -0700

    Update Problem_3.py

--- Week_1-_Python_Basics/Problem_Set_1/Problem_3.py
+++ Week_1-_Python_Basics/Problem_Set_1/Problem_3.py
@@ -10,9 +10,9 @@
 # In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print:
 # Longest substring in alphabetical order is: abc
 
-# Note: This problem may be challenging. We encourage you to work smart. If you've spent more than a few hours on this problem, we suggest
-# that you move on to a different part of the course. If you have time, come back to this problem after you've had a break and cleared
-# your head.
+# Note: This problem may be challenging. We encourage you to work smart. If you've spent more than a few hours on this problem, we
+# suggest that you move on to a different part of the course. If you have time, come back to this problem after you've had a break and
+# cleared your head.
 
 
 # Paste your code into this box
