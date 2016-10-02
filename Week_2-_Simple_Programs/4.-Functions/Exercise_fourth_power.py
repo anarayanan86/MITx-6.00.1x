a27af21e2ff309e3a14cc4f14f4af25456c1dda4
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-02 16:00:16 -0700

    Update Exercise_fourth_power.py

--- Week_2-_Simple_Programs/4.-Functions/Exercise_fourth_power.py
+++ Week_2-_Simple_Programs/4.-Functions/Exercise_fourth_power.py
@@ -5,8 +5,8 @@
 
 # Write a Python function, fourthPower, that takes in one number and returns that value raised to the fourth power.
 
-# You should use the square procedure that you defined in an earlier exercise exercise (you don't need to redefine square in this box; when
-# you call square, the grader will use our definition).
+# You should use the square procedure that you defined in an earlier exercise exercise (you don't need to redefine square in this box;
+# when you call square, the grader will use our definition).
 
 # This function takes in one number and returns one number.
 
