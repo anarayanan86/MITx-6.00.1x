7604f511200b3d94d776765e325eaad80e18c6d5
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-03 09:20:32 -0700

    Update Exercise_gcd_iter.py

--- Week_2-_Simple_Programs/4.-Functions/Exercise_gcd_iter.py
+++ Week_2-_Simple_Programs/4.-Functions/Exercise_gcd_iter.py
@@ -9,9 +9,9 @@
 # gcd(9, 12) = 3
 # gcd(17, 12) = 1
 
-# Write an iterative function, gcdIter(a, b), that implements this idea. One easy way to do this is to begin with a test value equal to the
-# smaller of the two input arguments, and iteratively reduce this test value by 1 until you either reach a case where the test divides both a
-# and b without remainder, or you reach 1.
+# Write an iterative function, gcdIter(a, b), that implements this idea. One easy way to do this is to begin with a test value equal to
+# the smaller of the two input arguments, and iteratively reduce this test value by 1 until you either reach a case where the test
+# divides both a and b without remainder, or you reach 1.
 
 
 def gcdIter(a, b):
