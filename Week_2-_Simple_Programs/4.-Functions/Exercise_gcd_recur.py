7811c75013540cc2cb244cc46ec80c4435163b55
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-03 09:22:22 -0700

    Update Exercise_gcd_recur.py

--- Week_2-_Simple_Programs/4.-Functions/Exercise_gcd_recur.py
+++ Week_2-_Simple_Programs/4.-Functions/Exercise_gcd_recur.py
@@ -9,11 +9,13 @@
 # gcd(9, 12) = 3
 # gcd(17, 12) = 1
 
-# A clever mathematical trick (due to Euclid) makes it easy to find greatest common divisors. Suppose that a and b are two positive integers:
+# A clever mathematical trick (due to Euclid) makes it easy to find greatest common divisors. Suppose that a and b are two positive
+# integers:
 # If b = 0, then the answer is a
 # Otherwise, gcd(a, b) is the same as gcd(b, a % b)
 
-# Write a function gcdRecur(a, b) that implements this idea recursively. This function takes in two positive integers and returns one integer.
+# Write a function gcdRecur(a, b) that implements this idea recursively. This function takes in two positive integers and returns one
+# integer.
 
 
 def gcdRecur(a, b):
