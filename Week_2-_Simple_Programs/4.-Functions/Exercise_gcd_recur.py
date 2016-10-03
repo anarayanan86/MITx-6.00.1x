a7e5db4bac45b38bb9e3cab56c88126effc1447c
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-03 09:21:41 -0700

    Add files via upload

---
+++ Week_2-_Simple_Programs/4.-Functions/Exercise_gcd_recur.py
@@ -0,0 +1,31 @@
+# Exercise: gcd recur
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 6 minutes
+
+# The greatest common divisor of two positive integers is the largest integer that divides each of them without remainder. For example,
+# gcd(2, 12) = 2
+# gcd(6, 12) = 6
+# gcd(9, 12) = 3
+# gcd(17, 12) = 1
+
+# A clever mathematical trick (due to Euclid) makes it easy to find greatest common divisors. Suppose that a and b are two positive integers:
+# If b = 0, then the answer is a
+# Otherwise, gcd(a, b) is the same as gcd(b, a % b)
+
+# Write a function gcdRecur(a, b) that implements this idea recursively. This function takes in two positive integers and returns one integer.
+
+
+def gcdRecur(a, b):
+    '''
+    a, b: positive integers
+
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Your code here
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return gcdRecur(b, a % b)
