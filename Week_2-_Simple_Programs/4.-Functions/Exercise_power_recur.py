11d04bfdbae6853ddde596b66a6c3d4facd52aec
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-06 09:55:03 -0700

    Update Exercise_power_recur.py

--- Week_2-_Simple_Programs/4.-Functions/Exercise_power_recur.py
+++ Week_2-_Simple_Programs/4.-Functions/Exercise_power_recur.py
@@ -3,11 +3,11 @@
 # (5/5 points)
 # ESTIMATED TIME TO COMPLETE: 7 minutes
 
-# In Problem 1, we computed an exponential by iteratively executing successive multiplications. We can use the same idea, but in a recursive
-# function.
+# In Problem 1, we computed an exponential by iteratively executing successive multiplications. We can use the same idea, but in a
+# recursive function.
 
-# Write a function recurPower(base, exp) which computes baseexp by recursively calling itself to solve a smaller version of the same problem,
-# and then multiplying the result by base to solve the initial problem.
+# Write a function recurPower(base, exp) which computes baseexp by recursively calling itself to solve a smaller version of the same
+# problem, and then multiplying the result by base to solve the initial problem.
 
 # This function should take in two values - base can be a float or an integer; exp will be an integer ≥0. It should return one numerical
 # value. Your code must be recursive - use of the ** operator or looping constructs is not allowed.
@@ -25,5 +25,5 @@ def recurPower(base, exp):
         return 1
     else:
         return base * recurPower(base, exp - 1)
-
+    
 # Correct
