0f5c586c584145b8d0e10e20870e000315cd7b6c
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-06 09:54:05 -0700

    Add files via upload

---
+++ Week_2-_Simple_Programs/4.-Functions/Exercise_odd.py
@@ -0,0 +1,22 @@
+# Exercise: odd
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 3 minutes
+
+# Write a Python function, odd, that takes in one number and returns True when the number is odd and False otherwise.
+
+# You should use the % (mod) operator, not if.
+
+# This function takes in one number and returns a boolean.
+
+
+def odd(x):
+    '''
+    x: int or float.
+
+    returns: True if x is odd, False otherwise
+    '''
+    # Your code here
+    return x % 2 != 0
+
+# Correct
---
+++ Week_2-_Simple_Programs/4.-Functions/Exercise_power_recur.py
@@ -0,0 +1,29 @@
+# Exercise: power recur
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 7 minutes
+
+# In Problem 1, we computed an exponential by iteratively executing successive multiplications. We can use the same idea, but in a recursive
+# function.
+
+# Write a function recurPower(base, exp) which computes baseexp by recursively calling itself to solve a smaller version of the same problem,
+# and then multiplying the result by base to solve the initial problem.
+
+# This function should take in two values - base can be a float or an integer; exp will be an integer ≥0. It should return one numerical
+# value. Your code must be recursive - use of the ** operator or looping constructs is not allowed.
+
+
+def recurPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    if exp == 0:
+        return 1
+    else:
+        return base * recurPower(base, exp - 1)
+
+# Correct
