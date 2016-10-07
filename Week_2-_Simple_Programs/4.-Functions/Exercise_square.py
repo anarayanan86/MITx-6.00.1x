fbb6e7aa9aecca841977863629bcb7c941beed83
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-07 08:47:43 -0700

    Add files via upload

---
+++ Week_2-_Simple_Programs/4.-Functions/Exercise_square.py
@@ -0,0 +1,18 @@
+# Exercise: square
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 3 minutes
+
+# Write a Python function, square, that takes in one number and returns the square of that number.
+
+# This function takes in one number and returns one number.
+
+
+def square(x):
+    '''
+    x: int or float.
+    '''
+    # Your code here
+    return x**2
+
+# Correct
