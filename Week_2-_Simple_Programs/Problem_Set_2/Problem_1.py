383045886bd1a36792131cbaca4db876649ed869
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-08 08:55:33 -0700

    Update Problem_1.py

--- Week_2-_Simple_Programs/Problem_Set_2/Problem_1.py
+++ Week_2-_Simple_Programs/Problem_Set_2/Problem_1.py
@@ -1,15 +1,16 @@
 # Problem 1 - Paying Debt off in a Year
 
 # (10/10 points)
-# Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit
-# card company each month.
+# Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the
+# credit card company each month.
 
 # The following variables contain values as described below:
 # balance - the outstanding balance on the credit card
 # annualInterestRate - annual interest rate as a decimal
 # monthlyPaymentRate - minimum monthly payment rate as a decimal
 
-# For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining balance. Be sure to print out no more than two decimal digits of accuracy - so print
+# For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
+# balance. Be sure to print out no more than two decimal digits of accuracy - so print
 
 # Remaining balance: 813.41
 # instead of
