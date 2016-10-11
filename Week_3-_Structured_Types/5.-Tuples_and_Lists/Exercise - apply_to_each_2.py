4ea3b819dda09d7fe42aaba50ac209054ef32c28
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-11 08:32:30 -0700

    Add files via upload

---
+++ Week_3-_Structured_Types/5.-Tuples_and_Lists/Exercise - apply_to_each_2.py
@@ -0,0 +1,14 @@
+# Exercise: apply to each 2
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 4 minutes
+
+
+# Your Code Here
+def addOne(b):
+        return b + 1
+
+applyToEach(testList, addOne)
+
+# [1, -4, 8, -9]
+# Correct
