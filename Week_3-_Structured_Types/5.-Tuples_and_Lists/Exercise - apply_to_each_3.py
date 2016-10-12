9db3ff6804cff867a1ab709fd22a7bd89980d82c
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-12 10:15:00 -0700

    Add files via upload

---
+++ Week_3-_Structured_Types/5.-Tuples_and_Lists/Exercise - apply_to_each_3.py
@@ -0,0 +1,12 @@
+# Exercise: apply to each 3
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 4 minutes
+
+
+# Your Code Here
+def squared(c):
+    return c**2
+
+applyToEach(testList, squared)
+# Correct
