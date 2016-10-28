f882a4ea0c73126f125ae1eb007c826f680a5da9
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-28 10:03:42 -0700

    Update Exercise:_int_set.py

--- Week_5-_Object_Oriented_Programming/9.-Classes_and_Inheritance/Exercise:_int_set.py
+++ Week_5-_Object_Oriented_Programming/9.-Classes_and_Inheritance/Exercise:_int_set.py
@@ -37,12 +37,12 @@ class intSet(object):
         return '{' + ','.join([str(e) for e in self.vals]) + '}'
         
 # Your task is to define the following two methods for the intSet class:
-# Define an intersect method that returns a new intSet containing elements that appear in both sets. In other words,
-# s1.intersect(s2)
-# would return a new intSet of integers that appear in both s1 and s2. Think carefully - what should happen if s1 and s2 have no
-# elements in common?
+#   1. Define an intersect method that returns a new intSet containing elements that appear in both sets. In other words,
+#      s1.intersect(s2)
+#      would return a new intSet of integers that appear in both s1 and s2. Think carefully - what should happen if s1 and s2 have no
+#      elements in common?
 
-# Add the appropriate method(s) so that len(s) returns the number of elements in s.
+#   2. Add the appropriate method(s) so that len(s) returns the number of elements in s.
 
 # Hint: look through the Python docs to figure out what you'll need to solve this problem.
 
