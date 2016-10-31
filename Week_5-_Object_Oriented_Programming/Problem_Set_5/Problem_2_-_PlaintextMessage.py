c7ced167f1c7253e29ab328ac8bb1b7b6e4ebaa1
Arjun Narayanan <anarayanan86@gmail.com>
2016-10-31 08:46:08 -0700

    Update Problem_2_-_PlaintextMessage.py

--- Week_5-_Object_Oriented_Programming/Problem_Set_5/Problem_2_-_PlaintextMessage.py
+++ Week_5-_Object_Oriented_Programming/Problem_Set_5/Problem_2_-_PlaintextMessage.py
@@ -3,16 +3,17 @@
 # For this problem, the graders will use our implementation of the Message class, so don't worry if you did not get the previous parts
 # correct.
 
-# PlaintextMessage is a subclass of Message and has methods to encode a string using a specified shift value. Our class will always create
-# an encoded version of the message, and will have methods for changing the encoding.
+# PlaintextMessage is a subclass of Message and has methods to encode a string using a specified shift value. Our class will always
+# create an encoded version of the message, and will have methods for changing the encoding.
 
 # Implement the methods in the class PlaintextMessage according to the specifications in ps6.py. The methods you should fill in are:
 # __init__(self, text, shift): Use the parent class constructor to make your code more concise.
 # The getter method get_shift(self)
-# The getter method get_encrypting_dict(self): This should return a COPY of self.encrypting_dict to prevent someone from mutating the original dictionary.
+# The getter method get_encrypting_dict(self): This should return a COPY of self.encrypting_dict to prevent someone from mutating the 
+# original dictionary.
 # The getter method get_message_text_encrypted(self)
-# change_shift(self, shift): Think about what other methods you can use to make this easier. It shouldn’t take more than a couple lines of
-# code.
+# change_shift(self, shift): Think about what other methods you can use to make this easier. It shouldn’t take more than a couple lines
+# of code.
 
 class PlaintextMessage(Message):
     def __init__(self, text, shift):
