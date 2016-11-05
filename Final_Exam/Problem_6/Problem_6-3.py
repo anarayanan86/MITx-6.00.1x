19a266616aa254773c61739afef5e96ee9858338
Arjun Narayanan <anarayanan86@gmail.com>
2016-11-05 09:10:40 -0700

    Create Problem_6-3.py

---
+++ Final_Exam/Problem_6/Problem_6-3.py
@@ -0,0 +1,25 @@
+# Problem 6-3
+# 15.0/15.0 points (graded)
+# You change your mind once more. You want to keep the behavior from Part 2, but now you would like:
+
+# >>> pe.say('the sky is blue')
+# Prof. eric says: I believe that eric says: the sky is blue 
+
+# >>> ae.say('the sky is blue')
+# Prof. eric says: It is obvious that I believe that eric says: the sky is blue 
+
+# Change the Professor class definition in order to achieve this. You may have to modify your implmentation for a previous part to get this
+# to work.
+
+# Paste ONLY the Professor class in the box below. Do not leave any debugging print statements.
+
+# For this question, you will not be able to see the test cases we run. This problem will test your ability to come up with your own test
+# cases.
+
+
+# Paste your class here
+class Professor(Lecturer): 
+    def say(self, stuff): 
+        return 'Prof. ' + self.name + ' says: ' + self.lecture(stuff)
+
+# Correct
